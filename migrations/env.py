52fc70927ac796512a437669b37ad44dad8aaf8b
Andrii Kushynskyi <akushyn@gmail.com>
2023-10-26 03:28:24 +0500

    clean alembic env.py

--- migrations/env.py
+++ migrations/env.py
@@ -15,15 +15,7 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def include_object(obj, name, type_, reflected, compare_to):
