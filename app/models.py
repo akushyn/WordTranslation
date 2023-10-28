f91bd2d98b9d11a29a73f44d9c443afbb9284fc1
Andrii Kushynskyi <akushyn@gmail.com>
2023-10-28 16:51:33 +0500

    add ExtraData, IncludeExtra models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from typing import Generic, TypeVar
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class TranslationRequest(BaseModel):
@@ -8,10 +8,40 @@ class TranslationRequest(BaseModel):
     source_lang: str | None = None
 
 
+class ExtraData(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    translation: list | None = None
+    all_translations: list | None = None
+    possible_translations: list | None = None
+    possible_mistakes: list | None = None
+    synonyms: list | None = None
+    definitions: list | None = None
+    examples: list | None = None
+
+
+class IncludeExtra(BaseModel):
+    result: bool = False
+    translation: bool = False
+    all_translations: bool = False
+    possible_translations: bool = False
+    possible_mistakes: bool = False
+    synonyms: bool = False
+    definitions: bool = False
+    examples: bool = False
+
+    @property
+    def true_attributes(self) -> set:
+        true_attributes = set(
+            attr for attr, value in self.model_dump().items() if value
+        )
+        return true_attributes
+
+
 class TranslationResponse(TranslationRequest):
     translated_word: str
     pronunciation: str | None = None
-    extra_data: dict
+    extra_data: ExtraData
 
 
 class TranslationCreate(TranslationResponse):
