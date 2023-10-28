be4792cd9d93650d8f4eb062fff2f0eb5528062d
Andrii Kushynskyi <akushyn@gmail.com>
2023-10-28 16:47:32 +0500

    add Translation hybrid properties

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, UniqueConstraint
 from sqlalchemy import Integer, String
 from sqlalchemy.dialects.postgresql import JSON
 from app.db.base import Base
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
 class Translation(Base):
@@ -21,3 +22,31 @@ class Translation(Base):
     source_lang = Column(String)
     target_lang = Column(String, nullable=False)
     extra_data = Column(JSON, nullable=False, default=lambda: {})
+
+    @hybrid_property
+    def translation(self):
+        return self.extra_data.get("translation")
+
+    @hybrid_property
+    def all_translations(self):
+        return self.extra_data.get("all_translations")
+
+    @hybrid_property
+    def possible_translations(self):
+        return self.extra_data.get("possible_translations")
+
+    @hybrid_property
+    def possible_mistakes(self):
+        return self.extra_data.get("possible_mistakes")
+
+    @hybrid_property
+    def synonyms(self):
+        return self.extra_data.get("synonyms")
+
+    @hybrid_property
+    def definitions(self):
+        return self.extra_data.get("definitions")
+
+    @hybrid_property
+    def examples(self):
+        return self.extra_data.get("examples")
