1605f4fa82c87da1bed967105a479f3cbf348872
Andrii Kushynskyi <akushyn@gmail.com>
2023-10-30 15:36:13 +0500

    fix database_url for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,15 @@ import pytest
 from faker import Faker
 from fastapi.testclient import TestClient
 
+from app.settings import settings
+
 faker = Faker()
 
 
 # Use autouse=True here to force read and apply app.settings
 @pytest.fixture(autouse=True)
 def app():
+    settings.database_url = "postgresql+asyncpg://test-db:translation@db/db"
     from app.main import app
 
     return app
