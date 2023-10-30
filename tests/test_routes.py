ef1518a791428b767f87630021e1ce74da09fb74
Andrii Kushynskyi <akushyn@gmail.com>
2023-10-30 15:06:34 +0500

    add api tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,14 +1,39 @@
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
+from fastapi import Response
 
-from app.models import TranslationRequest
+from app.models import ExtraData, PaginatedResponse, TranslationRequest
+from app.settings import settings
 
 
 @pytest.fixture
 def mock_request():
     request = TranslationRequest(word="challenge", target_lang="ru", source_lang="en")
     return request
+
+
+@pytest.fixture
+def mock_translation(faker):
+    translation = MagicMock()
+    translation.id = faker.random_int()
+    translation.word = faker.word()
+    translation.translated_word = faker.word()
+    translation.pronunciation = faker.word()
+    translation.source_lang = "en"
+    translation.target_lang = "ru"
+    translation.extra_data = ExtraData()
+
+    # include hybrid properties
+    translation.translation = None
+    translation.all_translations = None
+    translation.possible_translations = None
+    translation.possible_mistakes = None
+    translation.synonyms = None
+    translation.definitions = None
+    translation.examples = None
+
+    return translation
 
 
 @pytest.mark.real
@@ -163,3 +188,46 @@ def test_get_translation(mock_get_or_create_translation, mock_request, client):
         "id": 123,
     }
     mock_get_or_create_translation.assert_called_once_with(mock_request)
+
+
+@patch(
+    "app.api.routes.TranslationService.delete_translation",
+    new_callable=AsyncMock,
+)
+def test_delete_translation(mock_delete_translation, mock_request, client):
+    mock_delete_translation.return_value = Response(status_code=204)
+
+    response = client.delete("/api/translations/word/", params=mock_request.dict())
+
+    assert response.status_code == 204
+    assert response.text == ""
+    mock_delete_translation.assert_called_once_with(mock_request)
+
+
+@patch("app.api.routes.TranslationService.paginate", new_callable=AsyncMock)
+def test_get_translations(mock_paginate, client, mock_translation, faker):
+    mock_next = faker.url()
+    mock_previous = faker.url()
+    translations_count = 3
+    mock_translations = [mock_translation for _ in range(translations_count)]
+
+    mock_paginate.return_value = PaginatedResponse(
+        count=len(mock_translations),
+        page_num=1,
+        per_page=settings.pagination_per_page,
+        pages=1,
+        items=mock_translations,
+        next=mock_next,
+        previous=mock_previous,
+    )
+
+    response = client.get("/api/translations/")
+    response_payload = response.json()
+
+    assert response.status_code == 200
+    assert len(response_payload["items"]) == translations_count
+    assert response_payload["count"] == translations_count
+    assert response_payload["next"] == mock_next
+    assert response_payload["previous"] == mock_previous
+    assert response_payload["pages"] == 1
+    assert response_payload["per_page"] == settings.pagination_per_page
